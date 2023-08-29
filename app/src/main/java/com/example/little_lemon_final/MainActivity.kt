a596d5a8841110b015c51664a1a851876c0db727
Logan <coates.logan1125@gmail.com>
2023-08-29 14:32:44 -0600

    Starting project

---
+++ app/src/main/java/com/example/little_lemon_final/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.little_lemon_final
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
