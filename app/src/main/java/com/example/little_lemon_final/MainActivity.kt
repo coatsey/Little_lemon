00ce96c5683b10182f39c4ae70bc79db421ac97d
Logan <coates.logan1125@gmail.com>
2023-09-06 10:02:32 -0600

    updated gradle and Manifest

--- app/src/main/java/com/example/little_lemon_final/MainActivity.kt
+++ app/src/main/java/com/example/little_lemon_final/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.little_lemon_final
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.navigation.compose.rememberNavController
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
+        setContent {
+
+            val navController = rememberNavController()
+            NavigationComposable(context = applicationContext,navController = navController)
+        }
     }
 }
