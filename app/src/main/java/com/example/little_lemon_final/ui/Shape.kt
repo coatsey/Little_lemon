4b022f3176119de07616eba27d7aebec34d1882f
Logan <coates.logan1125@gmail.com>
2023-09-06 10:18:49 -0600

    finishing touches

---
+++ app/src/main/java/com/example/little_lemon_final/ui/Shape.kt
@@ -0,0 +1,12 @@
+package com.example.little_lemon_final.ui
+
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
